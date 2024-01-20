81dbe8735837ff437590ae95d9108449efc7959b
RAHUL KUVLEKAR <rahulkuvlekar007@gmail.com>
2024-01-20 21:43:31 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <h1 className="text-5xl font-extrabold text-purple-700 text-center">
+      Foto Finder App
+    </h1>
+  );
+};
+
+export default App;
